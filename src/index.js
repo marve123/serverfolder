d834f80fb96d138f5bdd338d03f68b8ba96dfdea
marve123 <eric.marvelboy@gmail.com>
2023-10-27 12:45:33 +0100

    allow localhost

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use(cors({
-    origin: [CLIENT_URL, 'http://localhost:5173/', 'https://morrnaire.tech/'],
+    origin: [CLIENT_URL, 'http://localhost:5173'],
     credentials: true,
   }));
 
