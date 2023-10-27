c0912d5140af3d679ca5c3c61ed4091ffb029dcb
marve123 <eric.marvelboy@gmail.com>
2023-10-27 15:19:38 +0100

    enable server final

--- src/index.js
+++ src/index.js
@@ -14,20 +14,20 @@ app.use(cookieParser())
 
 
 app.use(cors({
-    origin: CLIENT_URL,
+    origin: CLIENT_URL || 'http://localhost:5173' || 'https://www.morrnaire.tech',
     credentials: true,
   }));
 
 app.use(passport.initialize()) 
-app.use((req, res, next) => { 
-    res.setHeader('Access-Control-Allow-Origin', 'https://www.morrnaire.tech');
-    res.setHeader('Access-Control-Allow-Origin', 'https://morrnaire.tech');
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); 
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Authorization'); 
-    res.setHeader('Access-Control-Allow-Credentials', 'true'); 
-    next(); 
-  }); 
+// app.use((req, res, next) => { 
+//     res.setHeader('Access-Control-Allow-Origin', 'https://www.morrnaire.tech');
+//     res.setHeader('Access-Control-Allow-Origin', 'https://morrnaire.tech');
+//     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); 
+//     res.setHeader('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Authorization'); 
+//     res.setHeader('Access-Control-Allow-Credentials', 'true'); 
+//     next(); 
+//   }); 
 
   app.get('/', (req, res) => {
     res.send({message: 'Welcome to the API'})}
